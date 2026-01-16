be201c4803848348a8397b6dae4f512f38f9e063
H1Y128 <one6666hy@gmail.com>
2026-01-16 23:42:53 +0900

    Modify Index View with DB And Routing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,84 +15,24 @@
       <div class="col-2">
         <h2>食材</h2>
         <ul class="list-group">
-          <a href="#category-vegetable" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            野菜
-          </a>
-          <div class="collapse" id="category-vegetable">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">果菜</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">根菜</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">葉物</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">その他</a>
-            </div>
-          </div>
-          <a href="#category-fruit" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            果実
-          </a>
-          <div class="collapse" id="category-fruit">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">柑橘類</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">ベリー類</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">熱帯果実</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">その他</a>
-            </div>
-          </div>
-          <a href="#category-beans" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            豆類
-          </a>
-          <a href="#category-mushroom" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            きのこ類
-          </a>
-          <a href="#category-seaweed" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            海藻類
-          </a>
-          <a href="#category-meat" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            肉類
-          </a>
-          <div class="collapse" id="category-meat">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">豚肉</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">牛肉</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">鶏肉</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">その他</a>
-            </div>
-          </div>
-          <a href="#category-seafood" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            魚介類
-          </a>
-          <div class="collapse" id="category-seafood">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">魚</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">貝</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">甲殻</a>
-            </div>
-          </div>
-          <a href="#category-egg" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            卵類
-          </a>
-          <a href="#category-grains" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            穀物類
-          </a>
-          <div class="collapse" id="category-grains">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">米</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">麦</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">その他</a>
-            </div>
-          </div>
-          <a href="#category-dairy" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            乳類
-          </a>
-          <a href="#category-seasoning" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
-            調味料
-          </a>
-          <div class="collapse" id="category-seasoning">
-            <div class="list-group ms-3">
-              <a href="#" class="list-group-item list-group-item-action border-0">油</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">香辛料</a>
-              <a href="#" class="list-group-item list-group-item-action border-0">その他</a>
-            </div>
-          </div>
+          @foreach ($major_categories as $major_category)
+            <a href="#category-{{ $major_category->id }}" class="list-group-item list-group-item-action" data-bs-toggle="collapse">
+              {{ $major_category->name }}
+            </a>
+            @foreach ($categories as $category)
+              @if ($major_category->name === $category->name)
+                @continue
+              @endif
+              
+              @if ($category->major_category_id === $major_category->id)
+              <div class="collapse" id="category-{{ $major_category->id }}">
+                <div class="list-group ms-3">
+                  <a href="#" class="list-group-item list-group-item-action border-0">{{ $category->name }}</a>
+                </div>
+              </div>
+              @endif
+            @endforeach
+          @endforeach
         </ul>
 
         <h2>料理</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/index', [WebController::class, 'index'])->name('index');
